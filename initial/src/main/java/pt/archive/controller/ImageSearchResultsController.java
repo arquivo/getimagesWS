26744ede45640230fcc32040adc86690c41ae4d4
jnobre <joaoanobre@gmail.com>
2016-12-06 18:34:33 +0000

    [develop multithreading] work progress

--- initial/src/main/java/pt/archive/controller/ImageSearchResultsController.java
+++ initial/src/main/java/pt/archive/controller/ImageSearchResultsController.java
@@ -25,7 +25,15 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.util.List;
+import java.util.Set;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.ArrayList;
 
 @Configuration
@@ -33,7 +41,8 @@ import java.util.ArrayList;
 public class ImageSearchResultsController {
 	
 	private final Logger log = LoggerFactory.getLogger(this.getClass()); //Define the logger object for this class
-	private List<ImageSearchResult> imageResults = new ArrayList<ImageSearchResult>();
+	private List< ImageSearchResult > imageResults = new ArrayList< ImageSearchResult >( );
+	private String[] terms;
 	
 	/** Properties file application.properties**/
 	@Value( "${urlBase}" )
@@ -56,8 +65,11 @@ public class ImageSearchResultsController {
 	
 	@Value( "${hitsPerPage}" )
 	private String hitsPerPage;
-	/***************************/
 	
+	@Value( "${NThreads}" )
+	private int NThreads;
+	/***************************/
+
 	private List< ItemXML > resultOpenSearch;
 	
 	/**
@@ -71,25 +83,29 @@ public class ImageSearchResultsController {
     public ImageSearchResults getImages( @RequestParam(value="query", defaultValue="") String query,
     									 @RequestParam(value="stamp", defaultValue="19960101000000-20151022163016") String stamtp ) {
     	log.info( "New request query[" + query + "] stamp["+ stamtp +"]" );
+    	printProperties( );
     	
-    		
     	return new ImageSearchResults( getImageResults( query , stamtp ) );
     }
     
-    /* Method that calls the OpenSearchAPI to get the first 10 urls of the query
+    /* Method that calls the OpenSearchAPI to get the first N urls of the query
     * and Returns a list of Images
     */
     public List<ImageSearchResult> getImageResults( String query , String stamp ) {
     	String url;
-    	HTMLParser resultImg;
+    	ExecutorService pool = Executors.newFixedThreadPool( NThreads );
+    	CountDownLatch latch;
+    	Set< Future< List< ImageSearchResult > > > set = new HashSet< Future< List< ImageSearchResult > > >( );
+    	List< Callable< Object > > todo;
     	
- 		if( query == null || query.trim().equals( "" ) ) {
+    	if( query == null || query.trim( ).equals( "" ) ) {
  			log.warn("[ImageSearchResultsController][getImageResults] Query empty!");
  			imageResults.add( getErrorCode( "-1: query empty" ) ); 
- 			return Collections.emptyList();
+ 			return Collections.emptyList( );
  		}
  		
  		try {
+ 			terms = query.split( " " );
  			url = buildURL( query , stamp );
  			log.debug( "Teste input == " + URLEncoder.encode(query, "UTF-8").replace("+", "%20") 
  					+ " url == " + url );
@@ -100,26 +116,60 @@ public class ImageSearchResultsController {
 	 		myReader.parse( new InputSource(new URL( url ).openStream( ) ) );
 	 		resultOpenSearch = userhandler.getItems( );
 	 		
-	 		if( resultOpenSearch == null || resultOpenSearch.size( ) == 0 ) 
+	 		if( resultOpenSearch == null || resultOpenSearch.size( ) == 0 )  
 	 			return  Collections.emptyList();
 	 		
-	 		
+	 		log.debug( "[ImageSearchResultsController][getImageResults] OpenSearch result : " + resultOpenSearch.size( ) );
+	 		latch = new CountDownLatch( resultOpenSearch.size( ) );
+	 		todo  = new ArrayList< Callable < Object > >( resultOpenSearch.size( ) );
+	 		List< Callable< List< ImageSearchResult > > > callables = new ArrayList<>();
 	 		//Search information tag <img>
-	 		resultImg = new HTMLParser(  );
-	 		imageResults = resultImg.buildResponse( resultOpenSearch , numImgsbyUrl , hostGetImage , urldirectoriesImage );
+	 		for( ItemXML item : resultOpenSearch ) {
+	 			//Callable< List< ImageSearchResult > > callable = new HTMLParser( latch , item,  numImgsbyUrl , hostGetImage , urldirectoriesImage ) );;
+	 			callables.add( new HTMLParser( latch , item,  numImgsbyUrl , hostGetImage , urldirectoriesImage ) ); 
+	 			
+	 			//Future< List< ImageSearchResult > > future = pool.submit( callable );
+	 			//set.add( future );
+	 		}
 	 		
- 		} catch( UnsupportedEncodingException e ) {
- 			log.error( "[ImageSearchResultsController][getImageResults]", e);
+	 		List< Future< List< ImageSearchResult > > > answer = pool.invokeAll( callables );
+	 		
+	 		//get images result to search
+	 		for( Future< List< ImageSearchResult > >  future : answer ) {
+	 			if( future.isDone( ) ) {
+	 				List< ImageSearchResult > result = future.get( ); // wait for a processor to complete
+		 			if( result != null && !result.isEmpty( ) )
+		 				imageResults.addAll( result );
+	 			}
+	 		}
+	 		
+	        
+	 		log.info( "Request query[" + query + "] stamp["+ stamp +"] Number of results["+ imageResults.size( ) +"]" );
+	 		
+		} catch( InterruptedException e ) {
+ 			log.error( "[ImageSearchResultsController][getImageResults]", e );
+ 			imageResults.add( getErrorCode( "[ERROR] Internal Error" ) );
+ 		} catch( ExecutionException e1 ) {
+ 			log.error( "[ImageSearchResultsController][getImageResults]", e1 );
+ 			imageResults.add( getErrorCode( "[ERROR] Internal Error" ) );
+ 		} catch( UnsupportedEncodingException e2 ) {
+ 			log.error( "[ImageSearchResultsController][getImageResults]", e2 );
  			imageResults.add( getErrorCode( "[ERROR] -5: URL Encoder Error" ) );
- 		} catch( SAXException e1 ) {
- 			log.error( "[ImageSearchResultsController][getImageResults]", e1);
+ 		} catch( SAXException e3 ) {
+ 			log.error( "[ImageSearchResultsController][getImageResults]", e3 );
  			imageResults.add( getErrorCode( "[ERROR] -2: Parser Error" ) ); 
- 		} catch( MalformedURLException e2 ) {
- 			log.error( "[ImageSearchResultsController][getImageResults]", e2);
+ 		} catch( MalformedURLException e4 ) {
+ 			log.error( "[ImageSearchResultsController][getImageResults]", e4 );
  			imageResults.add( getErrorCode( "[ERROR] -3: URL OpenSearch Error" ) );
- 		} catch( IOException e4 ) {
- 			log.error( "[ImageSearchResultsController][getImageResults]", e4);
+ 		} catch( IOException e5 ) {
+ 			log.error( "[ImageSearchResultsController][getImageResults]", e5 );
  			imageResults.add( getErrorCode( "[ERROR] -4: IOException Error" ) );
+ 		}catch( Exception e6 ) {
+ 			log.error( "[ImageSearchResultsController][getImageResults]", e6 );
+ 			imageResults.add( getErrorCode( "[ERROR]: No images found" ) );
+ 		}finally{
+	 		if( pool != null )
+	 			pool.shutdown( ); //shut down the executor service now
  		}
  		
  		return imageResults;
@@ -151,6 +201,20 @@ public class ImageSearchResultsController {
     	ImageSearchResult result = new ImageSearchResult( );
     	result.setUrl( errorCode );
     	return result;
+    }
+    
+    private void printProperties( ){
+    	log.info( "********* Properties *********" );
+    	log.info( "	urlBase=" +urlBase );
+    	log.info( "	type=" +type );
+    	log.info( "	hitsPerSite=" +hitsPerSite);
+    	log.info( "	hitsPerPage=" +hitsPerPage );
+    	log.info( "	NThreads=" +NThreads );
+    	log.info( "	NumImgsbyUrl=" +numImgsbyUrl );
+    	log.info( "	HostGetImage=" +hostGetImage );
+    	log.info( "	urldirectoriesImage=" +urldirectoriesImage );
+    	log.info( "	NThreads=" +NThreads );
+    	log.info( "******************************" );
     }
     
 }
--- initial/src/main/java/pt/archive/utils/HTMLParser.java
+++ initial/src/main/java/pt/archive/utils/HTMLParser.java
@@ -1,7 +1,11 @@
 package pt.archive.utils;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CountDownLatch;
+
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -12,85 +16,99 @@ import org.slf4j.LoggerFactory;
 import pt.archive.model.ImageSearchResult;
 import pt.archive.model.ItemXML;
 
-public class HTMLParser {
+public class HTMLParser implements Callable< List< ImageSearchResult > > {
 	private final Logger log = LoggerFactory.getLogger( this.getClass( ) );
 	private Document doc;
+	private ItemXML itemtoSearch;
+	private int numImgsbyUrl;
+	private String hostImage;
+	private String urldirect;
+	private List< ImageSearchResult > resultsImg;
+	private CountDownLatch doneSignal;
 	
-	public HTMLParser(  ) { }
+	public HTMLParser( CountDownLatch doneSignal , ItemXML itemtoSearch , int numImgsbyUrl , String hostImage , String urldirct  ) { 
+		this.itemtoSearch 	= itemtoSearch;
+		this.numImgsbyUrl 	= numImgsbyUrl;
+		this.hostImage		= hostImage;
+		this.urldirect		= urldirct;
+		this.resultsImg 	= new ArrayList< >( ); 
+		this.doneSignal 	= doneSignal;
+	}
 	
-	public List< ImageSearchResult > buildResponse( List< ItemXML > urlOpenSearch , int numImgsbyUrl , String hostImage , String urldirct ) {
-		List< ImageSearchResult > result = new ArrayList< >( );
+	@Override
+	public List< ImageSearchResult > call( ) throws Exception {
+		buildResponse( ); //build response to search images per link
+		return resultsImg;
+	}
+
+	public void buildResponse( ) {
 		int countImg = 0;
+		String link = getLink( itemtoSearch.getUrl( ) , itemtoSearch.getTstamp( ) , hostImage.concat( urldirect ) );
 		
-		log.debug( "[buildResponse] size url = " + urlOpenSearch.size( ) );
-		for( ItemXML item : urlOpenSearch ) {
-			String link = getLink( item.getUrl( ) , item.getTstamp( ) , hostImage.concat( urldirct ) );
-			log.debug( "[HTMLParser][buildResponse] URL search = " + link );
-			try {
-				Connection.Response resp = Jsoup.connect( link )
-						.userAgent( "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.21 (KHTML, like Gecko) Chrome/19.0.1042.0 Safari/535.21" )
-						.timeout( 5000 )
-						.execute( );
-				doc = null;
-				if( resp.statusCode( ) == 200 ) 
-					doc = resp.parse( );
-				
-			} catch( Exception e ) {
-				log.error( "[Jsoup] get response ["+ item.getUrl( ) +"] exception = ", e );
-				continue;
-			}
+		log.debug( "[HTMLParser][buildResponse] URL search = " + link );
+		try {
+			Connection.Response resp = Jsoup.connect( link )
+					.userAgent( "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.21 (KHTML, like Gecko) Chrome/19.0.1042.0 Safari/535.21" )
+					.timeout( 5000 )
+					.execute( );
+			doc = null;
+			if( resp.statusCode( ) == 200 ) 
+				doc = resp.parse( );
 			
-			String title = doc.title( ); //get page title
-			Elements links = doc.select( "img" ); //get all tag <img />
-			for( Element imgItem : links) {
-					
-				if( numImgsbyUrl != -1 ) 
-					if( countImg == numImgsbyUrl ) break; 
-				
-				String src;
-				if( imgItem.attr( "src" ) != null && !imgItem.attr( "src" ).trim().equals( "" ) ) {
-					src  = imgItem.attr( "src" );
-					if( !src.startsWith( hostImage ) ) 
-						src = hostImage.concat( src );
-				}
-				else 
-					continue;
-				
-				String width;
-				if( imgItem.attr( "width" ) != null && !imgItem.attr( "width" ).trim().equals( "" ) )
-					width  = imgItem.attr( "width" );
-				else 
-					width = "";
-				
-				String height;
-				if( imgItem.attr( "height" ) != null && !imgItem.attr( "height" ).trim().equals( "" ) )
-					height  = imgItem.attr( "height" ); 
-				else
-					height = "";
-				
-				String alt;
-				if( imgItem.attr( "alt" ) != null && !imgItem.attr( "height" ).trim().equals( "" ) )
-					alt  = imgItem.attr( "alt" ); 
-				else 
-					alt = "";
-				
-				if( title == null || title.trim().equals( "" ) )
-					title = "";
-				
-				result.add( new ImageSearchResult(  src , width , height , alt , title , item.getUrl( ) , item.getTstamp( ) ) );
-				
-				log.debug( "[Images] source = " + imgItem.attr( "src" ) + " alt = " + imgItem.attr( "alt" ) 
-				          + " height = " + imgItem.attr( "height" ) + " width = " + imgItem.attr( "width" ) + " urlOriginal = " + item.getUrl( ) );
-				
-				if( numImgsbyUrl != -1) countImg++;
-				
-			}
-			countImg = 0;			
+		} catch( Exception e ) {
+			log.error( "[Jsoup] get response link["+ link +"] orignalURL["+ itemtoSearch.getUrl( ) +"] exception = ", e );
+			return;
 		}
 		
-		log.info( "Total de results = " + result.size( ) );
+		String title = doc.title( ); //get page title
+		Elements links = doc.select( "img" ); //get all tag <img />
+		for( Element imgItem : links ) {
+				
+			if( numImgsbyUrl != -1 ) 
+				if( countImg == numImgsbyUrl ) break; 
+			
+			String src;
+			if( imgItem.attr( "src" ) != null && !imgItem.attr( "src" ).trim().equals( "" ) ) {
+				src  = imgItem.attr( "src" );
+				if( !src.startsWith( hostImage ) ) 
+					src = hostImage.concat( src );
+			}
+			else 
+				continue;
+			
+			String width;
+			if( imgItem.attr( "width" ) != null && !imgItem.attr( "width" ).trim().equals( "" ) )
+				width  = imgItem.attr( "width" );
+			else 
+				width = "";
+			
+			String height;
+			if( imgItem.attr( "height" ) != null && !imgItem.attr( "height" ).trim().equals( "" ) )
+				height  = imgItem.attr( "height" ); 
+			else
+				height = "";
+			
+			String alt;
+			if( imgItem.attr( "alt" ) != null && !imgItem.attr( "height" ).trim().equals( "" ) )
+				alt  = imgItem.attr( "alt" ); 
+			else 
+				alt = "";
+			
+			if( title == null || title.trim().equals( "" ) )
+				title = "";
+			
+			resultsImg.add( new ImageSearchResult(  src , width , height , alt , title , itemtoSearch.getUrl( ) , itemtoSearch.getTstamp( ) ) );
+			
+			log.debug( "[Images] source = " + imgItem.attr( "src" ) + " alt = " + imgItem.attr( "alt" ) 
+			          + " height = " + imgItem.attr( "height" ) + " width = " + imgItem.attr( "width" ) + " urlOriginal = " + itemtoSearch.getUrl( ) );
+			
+			if( numImgsbyUrl != -1) countImg++;
+			
+		}
+		countImg = 0;			
 		
-		return result;
+		log.info( "Total de results = [" + resultsImg.size( ) + "] to url[" + link + "]" );
+		
 	}
 	
 	private String getLink( String url , long tstamp , String linkGetImage ) {
@@ -100,6 +118,19 @@ public class HTMLParser {
 				.concat( Constants.urlBarOP )
 				.concat( url );
 	}
-	
-	
+
+	public List< ImageSearchResult > getResultsImg( ) {
+		return resultsImg;
+	}
+
+	public void setResultsImg( List< ImageSearchResult > resultsImg ) {
+		this.resultsImg = resultsImg;
+	}
+
+	@Override
+	public String toString( ) {
+		return "HTMLParser [doc=" + doc.getElementsByAttribute( "title" ) + ", numImgsbyUrl=" + numImgsbyUrl + ", hostImage=" + hostImage + ", urldirect="
+				+ urldirect + "]";
+	}
+
 }
